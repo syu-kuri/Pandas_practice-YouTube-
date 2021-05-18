c4358ec60056868c47b1a2ba5adb566ad9566ab4
syukuri <souki.shinohara@outlook.jp>
2021-05-18 19:21:50 +0900

    9本目修正

--- 09_カラム名変更.py
+++ 09_カラム名変更.py
@@ -12,7 +12,20 @@ import pandas as pd
 df = pd.read_csv('weather.csv')
 
 df_columns = df.columns
-# ここまでは03_不要な列、行の削除で終了済み
+
+df = df[['年月日', 
+'平均気温(℃)', 
+'最高気温(℃)',
+'最低気温(℃)',
+'降水量の合計(mm)', 
+'最深積雪(cm)',
+'平均雲量(10分比)', 
+'平均蒸気圧(hPa)',
+'平均風速(m/s)', 
+'日照時間(時間)'
+]]
+
+# ここまでは05_任意の要素を取得で終了済み
 
 
 # --------------
@@ -30,7 +43,10 @@ df.columns = ['年月日',
               '平均風速', 
               '日照時間']
 
+print(df)
+
 # rename()：ピンポイントで特定のカラム名を変更
-df.rename(columns={
+df = df.rename(columns={
   '平均気温': '平均'
 })
+print(df)
